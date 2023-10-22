5a46be61b8b7cea21e1e0e3d6184c94e41fa86f1
vitalinho <kirillovitaly@mail.ru>
2023-10-22 19:32:38 +0300

    новый файл video.py

---
+++ src/video.py
@@ -0,0 +1,39 @@
+import os
+from googleapiclient.discovery import build
+
+
+class Video:
+    api_key = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def __init__(self, video_id):
+        """
+        Создает объект по id видео
+        """
+        self.video_id = video_id
+        video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                     id=video_id
+                                                     ).execute()
+        self.title = video_response['items'][0]['snippet']['title']
+        self.url = f'https://www.youtube.com/watch?v={video_id}'
+        self.view_count = video_response['items'][0]['statistics']['viewCount']
+        self.like_count = video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return self.title
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        """
+        Получает id плейлиста и создает из него объект по id видео
+        """
+        api_key = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
+                                                       part='contentDetails',
+                                                       maxResults=50,
+                                                       ).execute()
+        for video in playlist_videos['items']:
+            if video['contentDetails']['videoId'] == video_id:
+                super().__init__(video_id)
